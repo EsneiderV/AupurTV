4a02acb8a389c041beecf738d0d58e39dfeb7b2c
Johan Camilo <johanfg32@gmail.com>
2022-06-29 15:00:11 -0500

    .

--- view/usuario.php
+++ view/usuario.php
@@ -5,29 +5,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="stilos.css">
     <link rel="stylesheet" href="../controllers/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
     <title>Login</title>
 </head>
 
 <body class="usuario-body">
     <div class="usuario-login">
 
-        <div>
-            <h1>Bienvenido</h4>
+        <div class="usuario-item-login titulo">
+            <h1>BIENVENIDO</h1>
         </div>
-        <div>
+        <div class="usuario-item-login">
             <form action="" method="POST">
                 <input name="documento" type="text" placeholder="Documento" required> <br> <br>
                 <input name="clave" type="text" placeholder="Contraseña" required> <br> <br>
-                <button name="acceder" type="submit">Acceder</button>
+                <button name="acceder" type="submit" >Acceder</button>
             </form>
         </div>
 
 
-        <div>
-            <a href="">Recuperar contraseña</a>
-            <a href="">Volver</a>
+        <div class="usuario-item-login">
+            <a href="" class="usuario-enlaces">Recuperar contraseña</a>
+            <a href="" class="usuario-enlaces">Volver</a>
         </div>
 
 
