964c3f718b84483a4483936ab869824754560261
EsneiderV <neiderurrego221203@gmail.com>
2022-12-14 09:35:47 -0500

    .

--- view/administrador/admiEmpleadoDelMes.php
+++ view/administrador/admiEmpleadoDelMes.php
@@ -20,6 +20,22 @@ $anio = date('Y');
         </h3>
     </div>
 
+    <div class="div-seleccionar-año">
+        <form id="formularioCambiarAnio" action="" method="post">
+            <select class="select-año" id="SelectCambiarAnio" name="aniodiagrama" id="">
+                <option value="0">Seleccione año</option>
+                <?php
+                $consultaSelects = traerAniosQueTiene($conexion);
+                while ($consultaSelect = mysqli_fetch_array($consultaSelects)) {
+                ?>
+                    <option value="<?php echo $consultaSelect['anio'] ?>"><?php echo $consultaSelect['anio'] ?></option>
+                <?php
+                }
+                ?>
+            </select>
+        </form>
+    </div>
+
     <div class="admiEmpleadoDelMes-contenedor-empleados">
         <div class="div-empleados-por-mes">
             <h2 class="titulo-empleado-por-mes">Empleados por mes</h2>
@@ -30,21 +46,13 @@ $anio = date('Y');
             </div>
         </div>
 
-        <div class="div-seleccionar-año">
-            <form id="formularioCambiarAnio" action="" method="post">
-                <select class="select-año" id="SelectCambiarAnio" name="aniodiagrama" id="">
-                    <option value="0">Seleccione año</option>
-                    <?php
-                    $consultaSelects = traerAniosQueTiene($conexion);
-                    while ($consultaSelect = mysqli_fetch_array($consultaSelects)) {
-                    ?>
-                        <option value="<?php echo $consultaSelect['anio'] ?>"><?php echo $consultaSelect['anio'] ?></option>
-                    <?php
-                    }
-                    ?>
-                </select>
-            </form>
-        </div>
+        <div class="div-empleados-por-mes">
+            <h2 class="titulo-empleado-por-mes">Empleados por mes</h2>
 
+            <div class="div-mes-persona">
+                <div class="div-mesesDelAño">Enero :</div>
+                <div>Pepito de Jesus Perez Perez</div>
+            </div>
+        </div>
     </div>
 </div>
